2f646f964c52f00523430a658dcbd001e20ee0f0
bvanh <48560384+bvanh@users.noreply.github.com>
2020-11-11 15:26:59 +0700

    f

---
+++ api/routers.js
@@ -0,0 +1,11 @@
+"use strict";
+// let logApi = require("./controls/LogController");
+// let token = require("./controls/TokenControler");
+// let apiUser = require("./controls/UserController");
+// let apiNews = require("./controls/NewsController");
+// let multer = require("./models/upload");
+// let media = require("./controls/MediaController");
+module.exports = function (app) {
+  // app.route("/login").post(logApi.login);
+  // app.route("/token/renew").post(token.refreshToken);
+};
---
+++ api/services/connection.js
@@ -0,0 +1,59 @@
+const testing = {
+  main: {
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+    timezone: "+07:00",
+    port: "23306",
+  },
+  // log: {
+  //   database: process.env.DB_NAME_LOG_TEST || "log",
+  //   username: process.env.DB_USER || "root",
+  //   password: process.env.DB_PASS || "",
+  //   host: process.env.DB_HOST || "localhost",
+  //   dialect: "mysql",
+  //   timezone: "+07:00",
+  // },
+};
+
+const production = {
+  main: {
+    database: process.env.DB_NAME_MAIN_PROD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    timezone: "+07:00",
+  },
+  duo: {
+    database: process.env.DB_NAME_DUO_PROD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    timezone: "+07:00",
+  },
+  log: {
+    database: process.env.DB_NAME_LOG_PROD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    timezone: "+07:00",
+  },
+  logcash: {
+    database: process.env.DB_NAME_LOG_CASH_PROD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    timezone: "+07:00",
+  },
+};
+
+module.exports = {
+  testing,
+  production,
+};
