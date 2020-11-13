67748e48c846a179b92f95bc795ffc80e046f8ad
bvanh <48560384+bvanh@users.noreply.github.com>
2020-11-13 10:08:41 +0700

    d

--- api/controls/UserController.js
+++ api/controls/UserController.js
@@ -2,27 +2,8 @@
 const Users = require("../models/Users");
 const attributesUser = ["userid", "level"];
 const { Op } = require("sequelize");
-const typeSearch = {
-  1: "username",
-};
-const getPagingData = (data, page, limit) => {
-  //console.log(data)
-  const { count: totalItems, rows: listUsers } = data;
-  const currentPage = page ? +page : 0;
-  const totalPages = Math.ceil(totalItems / limit);
-
-  return { totalItems, totalPages, currentPage, listUsers };
-};
-const getPagination = (page, size) => {
-  const limit = size ? +size : 3;
-  const offset = page ? page * limit : 0;
-  return { limit, offset };
-};
 module.exports = {
   getUsers: async (req, res, next) => {
-    //console.log(req)
-    // const { page, size } = req.query;
-    // const { limit, offset } = getPagination(page, size);
     Users.findAndCountAll({
       attributes: attributesUser,
       order: [["level", "DESC"]],
