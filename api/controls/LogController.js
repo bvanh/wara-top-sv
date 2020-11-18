a53f5927cd41e48cf57bd4c965947777ba5da05a
bvanh <48560384+bvanh@users.noreply.github.com>
2020-11-18 16:58:01 +0700

    d

--- api/controls/LogController.js
+++ api/controls/LogController.js
@@ -9,26 +9,43 @@ const attributesCharges = [
   [sequelize.fn("sum", sequelize.col("cash")), "total_cash"],
 ];
 // UserInfo.belongsTo(LogCharges, { foreignKey: "userid" });
-// LogCharges.hasMany(UserInfo, { foreignKey: "userid" });
+// Log.hasMany(UserInfo, { foreignKey: "userid", as: "t_profiles" });
+// Log.sync()
 module.exports = {
   getUsersCharges: async (req, res, next) => {
     Log.findAll({
       attributes: attributesCharges,
       group: ["game_user_id"],
-      include: 'company',
+      // include: {
+      //   model: UserInfo,
+      //   as: "tags",
+      //   required: true,
+      // },
       where: {
         partner_id: "A5173E6F-A2FF-4424-88E9-DA532346D738",
         //   game_user_id: "201088",
       },
+      // limit: pageSize,
+      // offset: pageSize * (page - 1),
       //   limit: 35,
       offset: 0,
     })
-      .then((data) => {
+      .then(async (data) => {
+        const arrUserId = await data.map((val) => Number(val.game_user_id));
+        UserInfo.findAll({
+          attributes: ["nickname"],
+          where: {
+            userid: arrUserId,
+            //   game_user_id: "201088",
+          },
+        }).then((data1) => {
+          res.status(200).send(data1);
+        });
         // await data.sort(
         //   (a, b) => Number(a.total_cash) - Number(b.total_cash)
         // );
         // const dataFilter=data.filter((val)=>Number(val.total_cash))
-        res.status(200).send(data);
+        // res.status(200).send(arrUserId);
       })
       .catch((err) => {
         res.status(500).send({
--- api/models/Log.js
+++ api/models/Log.js
@@ -4,22 +4,30 @@ const squelize = require("../services/databases");
 const Log = squelize.log.define(
   "Logs",
   {
-    userid: Squelize.CHAR,
     game_user_id: Squelize.CHAR,
-    //   nickname:Squelize.CHAR
   },
-  { tableName: "log_partner_charges" }
+  {
+    tableName: "log_partner_charges",
+  }
 );
-// const UserInfo = squelize.master.define(
-//   "Users",
-//   {
-//     userid: Squelize.CHAR,
-//   },
-//   {
-//     tableName: "t_profiles",
-//   }
-// );
-Log.associate = function(models) {
-  Log.hasMany(models.User, { as: "employes" });
-};
-module.exports = { Log };
+const UserInfo = squelize.master.define(
+  "Users",
+  {
+    userid: Squelize.CHAR,
+  },
+  {
+    tableName: "t_profiles",
+  }
+);
+// Log.belongsToMany(UserInfo, {
+//   through: "tutorial_tag",
+//   as: "tags",
+//   foreignKey: "game_user_id",
+// });
+
+// UserInfo.belongsToMany(Log, {
+//   through: "tutorial_tag",
+//   as: "tutorials",
+//   foreignKey: "userid",
+// });
+module.exports = { Log, UserInfo };
